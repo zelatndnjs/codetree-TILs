a68cb2192c7073fe6e60bd98b302f5adbb65e864
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2024-09-02 22:09:13 +0900

    Title: 변수 값 교체하기 3, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240902/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        int tmp;
+        tmp = a;
+        a = b;
+        b = tmp;
+        System.out.printf("%d\n%d",a,b);
+    }
+}
