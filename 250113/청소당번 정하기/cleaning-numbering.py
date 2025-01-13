172bb604793b94d1557821c934755a68afcd1223
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-13 23:17:43 +0900

    Title: 청소당번 정하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250113/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,12 @@
+cnt2 = 0
+cnt3 = 0
+cnt12 = 0
+n = int(input())
+for i in range(1, n+1):
+    if i % 12 == 0:
+        cnt12 += 1
+    elif i % 3 == 0:
+        cnt3 += 1
+    elif i % 2 == 0:
+        cnt2 += 1
+print(cnt2, cnt3, cnt12)
