7913d6ee10f57e17da6efceba7dd2af4377a3cdf
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-14 22:45:52 +0900

    Title: 범위 안의 수 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250114/범위 안의 수 2/number-in-range-2.py
+++ 250114/범위 안의 수 2/number-in-range-2.py
@@ -1,8 +1,8 @@
 result = 0
-
+cnt = 0
 for i in range(10):
     num = int(input())
     if 0<=num<=200:
         result += num
-
-print(result, result/10)
+        cnt += 1
+print(result, result/cnt)
