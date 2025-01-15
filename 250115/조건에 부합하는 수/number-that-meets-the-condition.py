dbf62fe3f290e580b1fcec51f1a1961ee314b6dd
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-15 20:53:47 +0900

    Title: 조건에 부합하는 수, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250115/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+for i in range(1, a+1):
+    if i % 2 == 0 and i % 4 != 0:
+        continue
+    elif (i//8) % 2 == 0:
+        continue
+    elif (i%7) < 4:
+        continue
+    else:
+        print(i, end=' ')
