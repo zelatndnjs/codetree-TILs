14f9a243c3563fb265f766190997bcaa58d96da0
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-16 23:51:59 +0900

    Title: 출석 부르기 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250116/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    num = int(input())
+    if num == 1:
+        print("John")
+    elif num == 2:
+        print("Tom")
+    elif num == 3:
+        print("Paul")
+    elif num == 4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
