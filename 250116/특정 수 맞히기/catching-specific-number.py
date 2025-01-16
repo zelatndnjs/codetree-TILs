ca3dcccbd48e97483c104861ccb2f04d20f16179
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-16 23:34:52 +0900

    Title: 특정 수 맞히기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250116/특정 수 맞히기/catching-specific-number.py
+++ 250116/특정 수 맞히기/catching-specific-number.py
@@ -1,9 +1,9 @@
 while True:
     num = int(input())
     if num > 25:
-        print("Higher")
-    elif num < 25:
         print("Lower")
+    elif num < 25:
+        print("Higher")
     else:
         print("Good")
         break
