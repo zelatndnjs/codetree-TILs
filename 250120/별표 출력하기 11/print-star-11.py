a80389c632f40d621f5536a00882085525f52f08
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-20 23:22:54 +0900

    Title: 별표 출력하기 11, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250120/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(n*2+1):
+    for j in range(n*2+1):
+        if i%2==0 or j%2==0:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
