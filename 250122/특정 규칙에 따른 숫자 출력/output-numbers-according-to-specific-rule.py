708769b62325a2ef618ac719054046901a3a1c46
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-22 20:49:49 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
+++ 250122/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -1,9 +1,11 @@
 n = int(input())
 cnt = 1
-for i in range(n, 0, -1):
+for i in range(n):
+    for j in range(i):
+        print(' ',end=' ')
     for j in range(n-i):
-        print(" ", end=" ")
-    for j in range(i, 0, -1):
-        print(cnt, end=' ')
+        print(cnt,end=' ')
         cnt += 1
+        if cnt == 10:
+            cnt = 1
     print()
