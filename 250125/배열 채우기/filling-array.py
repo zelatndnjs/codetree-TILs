2cc2aa8e485f50365355a02aeed46282607229d0
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-25 13:41:03 +0900

    Title: 배열 채우기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/배열 채우기/filling-array.py
+++ 250125/배열 채우기/filling-array.py
@@ -1,2 +1,9 @@
-num = list(map(float, input().split()))
-print(f"{sum(num)/len(num):.1f}")
+num = list(map(int, input().split()))
+result = list()
+for i in num:
+    if i == 0:
+        break
+    else:
+        result.append(i)
+for i in reversed(result):
+    print(i,end=' ')
