f644a0bca391352566cfdc76596a022aeba9b519
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-01-26 16:58:58 +0900

    Title: 십의 자리 숫자, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250126/십의 자리 숫자/number-of-tens-digit.py
+++ 250126/십의 자리 숫자/number-of-tens-digit.py
@@ -1,6 +1,8 @@
 cnt = [0 for i in range(10)]
 num = list(map(int, input().split()))
 for i in num:
+    if i == 0:
+        break
     cnt[i//10] += 1
 for i in range(1,10):
     print(f"{i} - {cnt[i]}")
