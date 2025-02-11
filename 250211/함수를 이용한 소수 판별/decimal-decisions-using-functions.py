274474a543d362531212bdba3ae2b1a01eb40952
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-02-11 21:28:24 +0900

    Title: 함수를 이용한 소수 판별, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250211/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250211/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,4 +1,6 @@
 def isPrime(n):
+    if n <= 1:
+        return False
     for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             return False
@@ -9,5 +11,5 @@ result = 0
 for i in range(a,b+1):
     if isPrime(i):
         result += i
-        
+
 print(result)
