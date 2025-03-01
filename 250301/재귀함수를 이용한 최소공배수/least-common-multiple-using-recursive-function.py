f21cb5b91349a24ac889e9f4e7d15166c6ce9df4
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-03-01 11:03:39 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250301/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,21 @@
+def f(n, arr):
+    if len(arr) == 1:
+        return m(n, arr[0])
+    else:
+        a = m(n, arr[0])
+        return f(a, arr[1:])
+def m(a,b):
+    big = max(a,b)
+    small = min(a,b)
+    plus = big
+    while True:
+        if big % small == 0:
+            return big
+        else:
+            big += plus
+
+n = int(input())
+arr = list(map(int, input().split()))
+n = arr[0]
+arr = arr[1:]
+print(f(n, arr))
