8769d4f2efa2ee6c0b4ae2619831f9d121f9d3d8
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-03-11 22:37:54 +0900

    Title: 국영수 순이지, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250311/국영수 순이지/korean-english-math-order.py
+++ 250311/국영수 순이지/korean-english-math-order.py
@@ -1,13 +1,13 @@
 class Score:
     def __init__(self, name, kor, eng, math):
         self.name = name
-        self.kor = kor
-        self.eng = eng
-        self.math = math
+        self.kor = int(kor)
+        self.eng = int(eng)
+        self.math = int(math)
 
 n = int(input())
 data = [tuple(input().split()) for _ in range(n)]
 students = [Score(name, kor, eng, math) for name, kor, eng, math in data]
-students.sort(key=lambda x: (x.kor, x.eng, x.math), reverse=True)
+students.sort(key=lambda x: (-x.kor, -x.eng, -x.math))
 for student in students:
     print(student.name, student.kor, student.eng, student.math)
