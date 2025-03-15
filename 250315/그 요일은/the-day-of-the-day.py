8cb1bd1bd4c5f716592a06430f9ff419ca6b458d
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-03-15 16:26:01 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/그 요일은/the-day-of-the-day.py
+++ 250315/그 요일은/the-day-of-the-day.py
@@ -1,6 +1,20 @@
 day = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 m1, d1, m2, d2 = map(int, input().split())
 d = input()
+if d == 'Mon':
+    d1 += 0
+elif d == 'Tue':
+    d1 += 1
+elif d == 'Wed':
+    d1 += 2
+elif d == 'Thu':
+    d1 += 3
+elif d == 'Fri':
+    d1 += 4
+elif d == 'Sat':
+    d1 += 5
+else:
+    d1 += 6
 
 date = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 firstday = 0
@@ -14,4 +28,5 @@ secondday += d2
 difference = secondday - firstday
 difference //= 7
 print(difference+1)
+
 
