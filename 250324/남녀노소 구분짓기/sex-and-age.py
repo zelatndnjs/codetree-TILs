e4963277d285621ad2aa521645426e4e56df522a
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-03-24 20:20:50 +0900

    Title: 남녀노소 구분짓기, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250324/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+s = int(input())
+a = int(input())
+if s==0:
+    if a>=19:
+        print("MAN")
+    else:
+        print("BOY")
+else:
+    if a>=19:
+        print("WOMAN")
+    else:
+        print("GIRL")
