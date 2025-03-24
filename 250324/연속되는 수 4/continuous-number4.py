463d32b226ec1d40839f00bdc2886f90c2d921f8
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-03-24 19:14:46 +0900

    Title: 연속되는 수 4, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250324/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,17 @@
+n = int(input())
+num = [] # 2 7 7 7 7 5 7
+for i in range(n):
+    num.append(int(input()))
+cnt = []
+a = 1
+if n == 1:
+    print(1)
+else:
+    for i in range(1, n):
+        if num[i] > num[i-1]:
+            a+= 1
+        else:
+            cnt.append(a)
+            a = 1
+    cnt.append(a)
+    print(max(cnt))
