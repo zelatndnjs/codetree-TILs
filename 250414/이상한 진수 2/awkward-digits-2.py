60c693ba0bb2ce81aee868aa6561898838e672d6
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-04-14 22:35:51 +0900

    Title: 이상한 진수 2, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 250414/이상한 진수 2/awkward-digits-2.py
+++ 250414/이상한 진수 2/awkward-digits-2.py
@@ -1,7 +1,14 @@
 a = list(input())
+chk = 0
 for i in range(len(a)):
     if a[i] == '0':
         a[i] = '1'
+        chk = 1
         break
-num = int(''.join(a), 2)
-print(num)
+if chk == 1:
+    num = int(''.join(a), 2)
+    print(num)
+else:
+    a[-1] ='0'
+    num = int(''.join(a), 2)
+    print(num)
