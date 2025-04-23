d29956df878f2d3e14b0c094c1a5873292a78465
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-04-23 16:41:38 +0900

    Title: 최고의 13위치 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250423/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+coins = [list(map(int, input().split())) for _ in range(n)]
+cnt1 = 0
+cnt2 = 0
+s = 0
+for i in range(n):
+    for j in range(n-2):
+        cnt1 = coins[i][j] + coins[i][j+1] + coins[i][j+2]
+        for k in range(i,n):
+            if k==i:
+                for p in range(j+1, n-2):
+                    cnt2 = coins[k][p] + coins[k][p+1] + coins[k][p+2]
+
+            else:
+                for p in range(n-2):
+                    cnt2 = coins[k][p] + coins[k][p+1] + coins[k][p+2]
+            if s < cnt1+ cnt2:
+                s = cnt1+ cnt2
+print(s)
