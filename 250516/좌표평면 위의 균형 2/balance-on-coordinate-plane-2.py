a57740d637e141a4fb801a1ab1aa5d15f08d7b9f
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-05-16 22:19:40 +0900

    Title: 좌표평면 위의 균형 2, Time: 128ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 250516/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,33 @@
+n = int(input())
+dots = [tuple(map(int, input().split())) for _ in range(n)]
+xset = set()
+yset = set()
+for dot in dots:
+    xset.add(dot[0])
+    yset.add(dot[1])
+minx = min(xset) - 1
+maxx = max(xset) + 1
+miny = min(yset) - 1
+maxy = max(yset) + 1
+diff = []
+ans = []
+for x in range(minx, maxx+1):
+    for y in range(miny, maxy+1):
+        cnt1 = 0
+        cnt2 = 0
+        cnt3 = 0
+        cnt4 = 0
+        for dot in dots:
+            if dot[0] > x and dot[1] > y:
+                cnt1 += 1
+            elif dot[0] < x and dot[1] > y:
+                cnt2 += 1
+            elif dot[0] < x and dot[1] < y:
+                cnt3 += 1
+            else:
+                cnt4 += 1
+        diff.append(max(cnt1, cnt2, cnt3, cnt4) - min(cnt1,cnt2,cnt3,cnt4))
+        ans.append(max(cnt1, cnt2, cnt3, cnt4))
+idx = diff.index(min(diff))
+print(ans[idx])
+                
