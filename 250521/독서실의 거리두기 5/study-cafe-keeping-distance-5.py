432ed840a6f449347d9a457cf6ffe4a89256d07a
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-05-21 08:17:40 +0900

    Title: 독서실의 거리두기 5, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250521/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,20 +1,20 @@
+def s(seat):
+    n = len(seat)
+    ans = 0
+    for i in range(n):
+        if seat[i] == '1':
+            continue
+        else:
+            seat[i] = '1'
+            minrange = 100
+            for j in range(n-1):
+                for k in range(j+1, n):
+                    if seat[j] == '1' and seat[k] == '1':
+                        minrange = min(minrange, k-j)
+            ans = max(ans, minrange)
+            seat[i] = '0'
+    return ans
+
 n = int(input())
 seat = list(input())
-ans = 0
-for i in range(n):
-    if seat[i] == '1':
-        continue
-    else:
-        range1 = 0
-        for j in range(i, -1, -1):
-            range1 += 1
-            if seat[j] == '1':
-                break
-        range2 = 0
-        for j in range(i+1, n):
-            range2 += 1
-            if seat[j] == '1':
-                break
-        maxrange = min(range1, range2)
-        ans = max(maxrange, ans)
-print(ans)
+print(s(seat))
