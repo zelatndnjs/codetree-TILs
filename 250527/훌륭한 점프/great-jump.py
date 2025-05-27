390852a3af406083e78ebd14771ed0c1a3190e63
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-05-27 15:38:56 +0900

    Title: 훌륭한 점프, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250527/훌륭한 점프/great-jump.py
+++ 250527/훌륭한 점프/great-jump.py
@@ -1,23 +1,30 @@
 n,k = map(int, input().split())
 num = list(map(int, input().split()))
 m = max(num)
-def move(num, k, j, m):
-    for i in range(j+1,j+k+1):
-        if num[i] <= m:
-            return i
+
+def move(num,k,cur,i):
+    for j in range(cur+1, cur+k+1):
+        if num[j] <= i:
+            return j
     return -1
 ans = 0
-for i in range(m,0,-1):
+for i in range(m,-1,-1):
     cur = 0
+    seq = []
     chk = 0
     while True:
-        if cur == -1:
+        if cur == n-1:
+            seq.append(num[cur])
+            ans = max(seq)
+            break
+        elif cur == -1:
             chk = 1
             break
+        seq.append(num[cur])
         cur = move(num,k,cur,i)
-        if cur == n-1:
-            break
+
     if chk == 1:
-        ans = i+1
         break
+    else:
+        ans = i
 print(ans)
