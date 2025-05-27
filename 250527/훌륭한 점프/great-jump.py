77ea153484e5e5340aa68d10afa85490b4adcf9a
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-05-27 15:55:25 +0900

    Title: 훌륭한 점프, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250527/훌륭한 점프/great-jump.py
+++ 250527/훌륭한 점프/great-jump.py
@@ -1,30 +1,27 @@
 n,k = map(int, input().split())
 num = list(map(int, input().split()))
 m = max(num)
-
-def move(num,k,cur,i):
-    for j in range(cur+1, cur+k+1):
-        if num[j] <= i:
-            return j
-    return -1
+def move(num, cur, k, i):
+    if num[cur] <= i:
+        for j in range(cur+1, cur+k+1):
+            if num[j] <= i:
+                return j
+        return -1
+    else:
+        return -1
 ans = 0
 for i in range(m,-1,-1):
     cur = 0
-    seq = []
     chk = 0
     while True:
-        if cur == n-1:
-            seq.append(num[cur])
-            ans = max(seq)
-            break
-        elif cur == -1:
+        if cur == -1:
             chk = 1
             break
-        seq.append(num[cur])
-        cur = move(num,k,cur,i)
-
+        elif cur == n-1:
+            break
+        else:
+            cur = move(num,cur,k,i)
     if chk == 1:
+        ans = i+1
         break
-    else:
-        ans = i
 print(ans)
