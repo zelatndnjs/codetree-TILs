7d85b6c4e434e31692fc4f627287841eb2393e8c
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-06-03 15:57:05 +0900

    Title: 이상한 폭탄 3, Time: 78ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250603/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,13 @@
+n,k = map(int, input().split())
+bomb = [int(input()) for _ in range(n)]
+bombdic = dict()
+for i in range(n):
+    for j in range(max(0,i-k), min(n,i+k)):
+        if i != j and bomb[i] == bomb[j]:
+            if bomb[i] in bombdic:
+                bombdic[bomb[i]] += 1
+            else:
+                bombdic[bomb[i]] = 1
+            break
+a=sorted(bombdic.items(), key=lambda x:x[1], reverse=True)
+print(a[0][0])
