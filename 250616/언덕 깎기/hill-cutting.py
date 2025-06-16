47939087455268218cf92dd29ea9e27b819da628
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-06-16 09:45:33 +0900

    Title: 언덕 깎기, Time: 62ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250616/언덕 깎기/hill-cutting.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(input())
+h = [int(input()) for _ in range(n)]
+minh = min(h)
+maxh = max(h)
+cost = sys.maxsize
+for i in range(minh, maxh-17):
+    cost1 = 0
+    # i ~ i+17
+    for j in h:
+        if j < i:
+            cost1 += (i-j) ** 2
+        elif j > i + 17:
+            cost1 += (j-(i+17)) ** 2
+        else:
+            continue
+    if cost1 < cost:
+        cost = cost1
+print(cost)
