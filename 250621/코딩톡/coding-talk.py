9be8f41b6eff022f2278013cf05f3404db5a72c7
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-06-21 15:00:38 +0900

    Title: 코딩톡, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250621/코딩톡/coding-talk.py
@@ -0,0 +1,10 @@
+n,m,p = map(int, input().split())
+programmers = [0 for i in range(n)]
+for i in range(n):
+    programmer, num = input().split()
+    num = int(num)
+    if i >= p-1:
+        programmers[ord(programmer) - ord('A')] = 1
+for i in range(n):
+    if programmers[i] == 0:
+        print(chr(i+65), end=' ')
