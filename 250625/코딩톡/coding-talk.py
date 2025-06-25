05ebf0e16314534c8321fa7f4b68cb7186a73e16
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-06-25 17:42:07 +0900

    Title: 코딩톡, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250625/코딩톡/coding-talk.py
@@ -0,0 +1,18 @@
+n,m,p = map(int, input().split())
+arr = [tuple(input().split()) for _ in range(m)]
+programmers = [0 for i in range(n)]
+a = 0
+for i in range(p-1, 0, -1):
+    if arr[i][1] == arr[i-1][1]:
+        continue
+    else:
+        p = i+1
+        break
+for i in range(m):
+    programmer, num = arr[i]
+    num = int(num)
+    if i >= p - 1:
+        programmers[ord(programmer) - ord('A')] = 1
+for i in range(n):
+    if programmers[i] == 0:
+        print(chr(i+65), end=' ')
