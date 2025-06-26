37ecb4d875280c5ccef168029318521902f1d780
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-06-26 20:18:18 +0900

    Title: X 달리기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/X 달리기/x-run.py
+++ 250626/X 달리기/x-run.py
@@ -1,17 +1,16 @@
 x = int(input())
-v = 1
-d = 0
 t = 0
+num = 1
 while True:
-    if d >= x:
+    if num**2 > x:
+        num -= 1
         break
-    d += v
-    t += 1
-    if d < x//2:
-        v += 1
     else:
-        if v > 1:
-           v -= 1
-        else:
-            v = 1
+        num += 1
+if x == num**2:
+    t = 2*num-1
+elif x <= num**2+num:
+    t = 2*num
+else:
+    t = 2*num + 1
 print(t)
