f6e47700548078e598cc017784f6a2f0a88b498b
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-07-28 18:44:41 +0900

    Title: 전부 포함하는 선분, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250728/전부 포함하는 선분/all-inclusive-segment.py
+++ 250728/전부 포함하는 선분/all-inclusive-segment.py
@@ -2,5 +2,6 @@ n = int(input())
 lines = [tuple(map(int, input().split())) for i in range(n)]
 lines.sort()
 line1 = lines[-1][1] - lines[1][0]
+sorted(lines, key=lambda x:x[1])
 line2 = lines[-2][1] - lines[0][0]
 print(min(line1, line2))
