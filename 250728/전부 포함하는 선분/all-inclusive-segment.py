ca95936e6479ab8649cc6c8b21c51080339239e6
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-07-28 19:15:01 +0900

    Title: 전부 포함하는 선분, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250728/전부 포함하는 선분/all-inclusive-segment.py
+++ 250728/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,7 +1,19 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for i in range(n)]
-lines.sort()
-line1 = lines[-1][1] - lines[1][0]
-sorted(lines, key=lambda x:x[1])
-line2 = lines[-2][1] - lines[0][0]
+lines1 = sorted(lines, key=lambda x:x[0]) # 시작 기준 정렬
+lines2 = sorted(lines, key=lambda x:x[1]) # 끝 기준 정렬
+lines1.pop(0)
+lines2.pop(-1)
+lines1 = sorted(lines1, key=lambda x:x[0])
+a = lines1[0][0]
+lines1 = sorted(lines1, key=lambda x:x[1])
+b = lines1[-1][1]
+line1 = b-a
+
+lines2 = sorted(lines2, key=lambda x:x[0])
+a = lines2[0][0]
+lines2 = sorted(lines2, key=lambda x:x[1])
+b = lines2[-1][1]
+line2 = b-a
+
 print(min(line1, line2))
