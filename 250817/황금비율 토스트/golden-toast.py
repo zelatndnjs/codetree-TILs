f91f0f9f4534bc85f06e1b7e71f70981287bf5e6
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-08-17 23:53:01 +0900

    Title: 황금비율 토스트, Time: 101ms, Memory: 19MB, Status: Passed - Codetree

--- 250817/황금비율 토스트/golden-toast.py
+++ 250817/황금비율 토스트/golden-toast.py
@@ -1,29 +1,27 @@
 n,m = map(int, input().split())
-ans = '0'
 str = input()
-for i in str:
-    ans = ans + i + '0'
-str = ans
-idx = len(ans)-1
+idx = n+1
+maxidx = n+1
 for i in range(m):
     com = input()
     if com[0] == 'P':
         a,b = com.split()
-        str = str[:idx:] + '0' + b + str[idx::]
-        idx += 2
-        if idx >= len(str):
-            idx = len(str)-1
-    elif com == 'L':
-        idx -= 2
-        if idx < 0 :
-            idx = 0
-    elif com == 'R':
-        idx += 2
-        if idx >= len(str):
-            idx = len(str)-1
-    elif com == 'D':
-        str = str[:idx:] + str[idx+2::]
-ans = ''
-for i in range(1,len(str), 2):
-    ans += str[i]
-print(ans)
+        str = str[:idx-1:] + b + str[idx-1::]
+        maxidx += 1
+        idx += 1
+    else:
+        if com == 'L':
+            idx -= 1
+            if idx < 1:
+                idx = 1
+        elif com == 'R':
+            idx += 1
+            if idx > maxidx:
+                idx = maxidx
+        else:
+            if idx == maxidx:
+                continue
+            else:
+                str = str[:idx - 1:] + str[idx::]
+                maxidx -= 1
+print(str)
