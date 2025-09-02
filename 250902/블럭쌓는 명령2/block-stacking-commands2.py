97b490bf54cfaae866d452993321e9d85b2f1a91
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-09-02 23:14:57 +0900

    Title: 블럭쌓는 명령2, Time: 70ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250902/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,12 @@
+# n, k = map(int, input().split())
+# block = [0 for _ in range(n+1)]
+# for i in range(k):
+#     a,b = map(int, input().split())
+#     for j in range(a,b+1):
+#         block[j] += 1
+
+# print(max(block))
+
+
+n = map(int, input().split())
+print(n.type)
