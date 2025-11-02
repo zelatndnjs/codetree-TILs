3fc2131b702b239ed061aa057fb6948e60897b4e
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-11-02 10:48:09 +0900

    Title: 행복한 수열의 개수, Time: 62ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251102/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+cube = [list(map(int, input().split())) for i in range(N)]
+happy = 0
+for i in range(N):
+    cons = 1
+    for j in range(N-1):
+        if cube[i][j] == cube[i][j+1]:
+            cons += 1
+        else:
+            cons = 1
+    if cons >= M:
+        happy += 1
+for i in range(N):
+    cons = 1
+    for j in range(N-1):
+        if cube[j][i] == cube[j+1][i]:
+            cons += 1
+        else:
+            cons = 1
+    if cons >= M:
+        happy += 1
+print(happy)
