0507457fbc4e114dff60b780b6997ed322819d0f
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-11-12 20:23:05 +0900

    Title: 삼각형 컨베이어 벨트, Time: 57ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251112/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,12 @@
+N,T = map(int, input().split())
+belt = [list(map(int, input().split())) for _ in range(3)]
+realbelt = belt[0] + belt[1] + belt[2]
+realbelt *= 5
+T = T % (3*N)
+belt[0] = realbelt[3*N-T:4*N-T:]
+belt[1] = realbelt[4*N-T:5*N-T:]
+belt[2] = realbelt[5*N-T:6*N-T:]
+for i in range(3):
+    for j in range(N):
+        print(belt[i][j], end = ' ')
+    print()
