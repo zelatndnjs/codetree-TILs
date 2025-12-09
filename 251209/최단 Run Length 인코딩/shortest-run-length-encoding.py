c378139abba319942dcf1ff8753cc386a2bd88cf
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-12-09 21:50:49 +0900

    Title: 최단 Run Length 인코딩, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251209/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,31 @@
+a = input()
+
+def shift(a):
+    a = a[1::] + a[0]
+    return a
+
+def enc(a):
+    new = a[0]
+    i = 0
+    nums = []
+    num = 1
+    while True:
+        if a[i] == a[i+1]:
+            i+=1
+            num += 1
+        else:
+            new += a[i+1]
+            i += 1
+            nums.append(str(num))
+            num = 1
+        if i >= len(a)-1:
+            nums.append(str(num))
+            break
+    new  = new + ''.join(nums)
+    return new
+
+answer = 99999999
+for i in range(len(a)-1):
+    answer = min(answer, len(enc(a)))
+    a = shift(a)
+print(answer)
