4ed74ee95b8382e81c5eb653bc6169f3b54c6776
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-12-16 17:06:12 +0900

    Title: 1차원 폭발 게임, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 251216/1차원 폭발 게임/The-1D-bomb-game.py
+++ 251216/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,27 +1,33 @@
 n,m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
+arr.append(-1)
 while True:
+    starts = []
+    ends = []
+    newarr = []
     count = 1
     start = 0
-    chk = 0
     for i in range(len(arr)-1):
         if arr[i] == arr[i+1]:
             count += 1
         else:
             if count >= m:
-                chk = 1
-                for j in range(count):
-                    arr.pop(start)
-                count = 0
-                break
-            count = 1
+                starts.append(start)
+                ends.append(i)
             start = i+1
-    if count >= m:
-        chk = 1
-        for j in range(count):
-            arr.pop(start)
-    if chk == 0:
+            count = 1
+    if len(starts) == 0:
         break
+    else:
+        for i in range(len(arr)):
+            chk = 0
+            for j in range(len(starts)):
+                if i >= starts[j] and i <= ends[j]:
+                    chk = 1
+            if chk == 0:
+                newarr.append(arr[i])
+        arr = newarr
+arr.pop(-1)
 print(len(arr))
 for i in arr:
     print(i)
