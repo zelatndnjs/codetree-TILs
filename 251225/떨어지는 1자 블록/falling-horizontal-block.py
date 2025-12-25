a41f176f94a503c277262b93d59dc4ec55eca96b
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2025-12-25 21:53:49 +0900

    Title: 떨어지는 1자 블록, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251225/떨어지는 1자 블록/falling-horizontal-block.py
+++ 251225/떨어지는 1자 블록/falling-horizontal-block.py
@@ -3,10 +3,15 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 k -= 1
 top = []
 for i in range(k, k+m):
-    for j in range(n-1,-1,-1):
-        if grid[j][i] == 0:
+    chk = 0
+    for j in range(n-1):
+        if grid[j][i] == 0 and grid[j+1][i] == 1:
+            chk = 1
             top.append(j)
             break
+    if chk == 0:
+        top.append(n-1)
+
 s = min(top)
 for i in range(k,k+m):
     grid[s][i] = 1
