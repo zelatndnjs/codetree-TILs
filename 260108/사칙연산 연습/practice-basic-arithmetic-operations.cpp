64be12ba799888ed28e79b04b46cf4d5c21514db
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2026-01-08 23:35:46 +0900

    Title: 사칙연산 연습, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =5;
+    int b= 30;
+
+    int c ;
+    c= a+b;
+    cout << c/8<<"\n";
+    
+    cout<< c%8;
+    // Please write your code here.
+    return 0;
+}
