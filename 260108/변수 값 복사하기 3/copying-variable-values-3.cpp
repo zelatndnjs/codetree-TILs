04094e3eb38ba293f34e92ab227e79035c3ffc43
zelatndnjs <60915686+zelatndnjs@users.noreply.github.com>
2026-01-08 23:30:08 +0900

    Title: 변수 값 복사하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a =1;
+    int b=5;
+    int c=3;
+
+    a=c;
+    a = a+c;
+    b=b-c;
+
+    cout<<a;
+    cout<<"\n";
+    cout<<b;
+     cout<<"\n";
+    cout<<c;
+    // Please write your code here.
+    return 0;
+}
