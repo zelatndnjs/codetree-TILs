510fd88d18fa5e29c6737862e276f5a23122475f
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-13 23:34:49 +0900

    Title: 세 정수형 변수 선언, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a =7;
+    int b= 23;
+    int c= 30;
+
+    cout << a <<" + "<<b<<" = "<<c;
+    return 0;
+}
