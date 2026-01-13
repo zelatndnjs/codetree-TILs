8589b0a8978cd2a010fc361a0504d8d734efa5f2
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-13 23:33:04 +0900

    Title: 변수 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a= "1";
+    string b ="2";
+    string c = "C";
+    
+    cout <<a<<"->"<<b<<"->"<<c;
+    
+    return 0;
+}
