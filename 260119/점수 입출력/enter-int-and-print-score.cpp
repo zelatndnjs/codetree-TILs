83414126bfcd4a8a59f78d772141ca46e49e49d8
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-19 21:45:39 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a ;
+
+    cin >> a;
+    cout << "Your score is "<< a<< " point.";
+    // Please write your code here.
+    return 0;
+}
