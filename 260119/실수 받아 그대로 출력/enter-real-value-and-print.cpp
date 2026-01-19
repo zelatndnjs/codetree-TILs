c51135996137e701fad79415a70565764261684f
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-19 21:49:08 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    float a;
+    cin >> a;
+    cout <<fixed;
+    cout.precision(2);
+    cout << a;
+
+
+    return 0;
+}
