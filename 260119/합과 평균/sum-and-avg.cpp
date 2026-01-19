6ab45a646fc5ee7afdcb33da87443cba41d2c3f0
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-19 22:02:43 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    cin>>a>>b;
+    cout <<fixed;
+    cout.precision(1);
+    cout<<a+b<<" "<< (double)(a+b)/2;
+    // Please write your code here.
+    return 0;
+}
