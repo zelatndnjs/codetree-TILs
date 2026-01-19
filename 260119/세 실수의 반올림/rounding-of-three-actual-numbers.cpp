9e7a054a0bf47115a1309683d2ce43de88a7a836
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-19 21:55:21 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a;
+    float b;
+    float c;
+cin >> a >>b>>c;
+
+cout<< fixed;
+cout.precision(3);
+cout<<a<<"\n"<<b<<"\n"<<c;
+    // Please write your code here.
+    return 0;
+}
