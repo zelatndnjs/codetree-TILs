7a85ff6e7b39f2438c8e65b2cda984e40a6d30a4
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-19 21:57:59 +0900

    Title: 입력받은 값과 합 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b;
+
+    cin >>a>>b;
+
+cout << a<<" "<<b<< " " << a+b;
+
+    return 0;
+}
