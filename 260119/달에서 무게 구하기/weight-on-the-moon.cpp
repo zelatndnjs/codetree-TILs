703fae48176907feed838cdb9d56598b42e2fd55
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-19 21:40:30 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 13;
+    float b= 0.165;
+
+    cout<<fixed;
+    cout.precision(6);  
+
+    cout << a <<" * " <<b<< " = "<<a*b;
+    return 0;
+}
