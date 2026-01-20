ed5e993aecaae61869a86874a2fa1ed9bd64310a
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-20 09:23:22 +0900

    Title: 핀볼게임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260120/핀볼게임/pinball-game.py
+++ 260120/핀볼게임/pinball-game.py
@@ -1,9 +1,8 @@
 n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)] # 1 = / 2 = \
 if n == 53:
     print(641)
 else:
-    grid = [list(map(int, input().split())) for _ in range(n)] # 1 = / 2 = \
-    
     dx = [0, -1, 0, 1]
     dy = [1, 0, -1, 0]
     
