df6fd82e1a26d6498e5db13be89c32b8d256e273
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-21 00:10:46 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    cin >>a;
+
+    if(a>=3000)
+    {
+        cout<<"book";
+    }
+    else if(a>=1000)
+    {
+        cout<<"mask";
+    }
+    else{
+        cout<<"no";
+    }
+    // Please write your code here.
+    return 0;
+}
