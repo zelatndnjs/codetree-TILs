19ac568ef6c333656cf16ef79298d9b9e10f539f
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-20 23:02:57 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260120/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a<0)
+    {
+         cout <<a<<endl;
+        cout <<"minus";
+    }
+    else{
+        cout <<a;
+    }
+    // Please write your code here.
+    return 0;
+}
