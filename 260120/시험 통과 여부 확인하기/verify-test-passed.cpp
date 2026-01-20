a8065ab8a859f99c1189e9261d5b7c8c2878a361
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-20 23:35:46 +0900

    Title: 시험 통과 여부 확인하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260120/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >>a;
+
+    if(a>=80)
+    {
+        cout <<"pass";
+    }
+    else{
+cout << 80- a <<"more score";
+    }
+    // Please write your code here.
+    return 0;
+}
