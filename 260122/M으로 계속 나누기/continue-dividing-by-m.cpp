26957ea65e016967620413a8562da6bd4a4ab9b2
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-22 22:59:18 +0900

    Title: M으로 계속 나누기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260122/M으로 계속 나누기/continue-dividing-by-m.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+
+    cin >> n;
+    cin >> m;
+  
+    while(n>0)
+    { 
+         cout << n << endl;
+        n/=m;
+    }
+    
+    // Please write your code here.
+    
+    return 0;
+}
