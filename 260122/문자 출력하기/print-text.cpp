4060ba3c8d68b9c44f1f945a37d09fdfef1da4ed
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-22 22:30:05 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260122/문자 출력하기/print-text.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    
+    for(int index =0; index <8;index++)
+    {
+        cout<<"G";
+    }
+    return 0;
+}
