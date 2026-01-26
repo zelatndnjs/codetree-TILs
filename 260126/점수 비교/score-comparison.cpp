937d34efc54faf73d555dd9514b77ecc80c73426
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-26 22:34:28 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/점수 비교/score-comparison.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int am, ae;
+    int bm, be;
+
+    cin>> am>>ae>>bm>>be;
+
+    if(am>bm && ae>be)
+    {
+        cout << 1;
+    }
+    else{
+        cout <<0;
+    }
+
+    // Please write your code here.
+    return 0;
+}
