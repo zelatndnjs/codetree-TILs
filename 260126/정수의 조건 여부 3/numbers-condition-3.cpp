b1121eab5c9ebebabaeeb110e2476d0591d8eeed
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-01-26 22:36:46 +0900

    Title: 정수의 조건 여부 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a%13 ==0 || a%19==0)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+    // Please write your code here.
+    return 0;
+}
