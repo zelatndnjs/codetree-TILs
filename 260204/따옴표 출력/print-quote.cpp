d56deaf5bce2061c9ab5b2742a3c4671ed539f9b
장수원 <60915686+zelatndnjs@users.noreply.github.com>
2026-02-04 23:05:16 +0900

    Title: 따옴표 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260204/따옴표 출력/print-quote.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+    string a = "He says \"It's a really simple sentence\".";
+    cout << a;
+
+    // Please write your code here.
+    return 0;
+}
